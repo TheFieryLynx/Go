76c646ba83d1bc13090df381962ef289adc74285
My Name <myEmail@example.com>
2020-02-23 20:02:50 +0300

    Initial commit.

--- server.go
+++ server.go
@@ -1,18 +1,54 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
-type message string
+type person struct {
+	Name string `json:"Name"`
+	Date string `json:"Date"`
+	Data string `json:"Data"`
+}
 
-func (m message) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(resp, m)
+func randInt(min, max int) int {
+	return min + rand.Intn(max-min)
+}
+
+func randomString(len int) string {
+	sym := make([]byte, len)
+	for i := 0; i < len; i++ {
+		sym[i] = byte(randInt(97, 122))
+	}
+	return string(sym)
 }
 
 func main() {
-	msgHandler := message("Hello from Server!")
+	
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello from Server!")
+	})
+
+	http.HandleFunc("/info", func(w http.ResponseWriter, r *http.Request) {
+		var man person
+		man.Name = r.URL.Query().Get("name")
+		if len(man.Name) == 0 {
+			man.Name = "anon"
+		} else {
+			man.Date = time.Now().Format("02.01.2006 15:04:05")
+			man.Data = randomString(20)
+		}
+		fmt.Fprintf(w, "Name: %s\nDate: %s\nData: %s\n\n\n", man.Name, man.Date, man.Data)
+
+		var jsonData []byte
+		jsonData,_ = json.Marshal(man)
+		fmt.Fprintf(w, "%s", string(jsonData))
+
+	})
+
 	fmt.Println("Starting...")
-	http.ListenAndServe("localhost:8080", msgHandler) //msgHandler - интерфейс type Handler interface
+	http.ListenAndServe("localhost:8080", nil) //msgHandler - интерфейс type Handler interface
 }
